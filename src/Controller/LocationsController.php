aee40a5335f7ed596e48a9c218dd6f43aa03614d
msirisko <mirja.sirisko@adesso.de>
2022-05-25 06:42:25 -0700

    POST added

--- src/Controller/LocationsController.php
+++ src/Controller/LocationsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Locations;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Service\JsonSerializer;
@@ -12,11 +13,22 @@ use App\Service\JsonSerializer;
 class LocationsController extends AbstractController
 {
 
- #[Route('/locations/', name: 'get_locations_all', methods: ['GET', 'HEAD'])]
+ #[Route('/locations/', name: 'show_locations_all', methods: ['GET', 'HEAD'])]
     public function showLocations(ManagerRegistry $doctrine, JsonSerializer $JsonSerializer): Response
 {
     $data = $doctrine->getRepository(Locations::class)->findAll();
     $jsonContent = $JsonSerializer->serializeJson($data);
     return new Response($jsonContent);
 }
+
+    #[Route('/locations/', name: 'add_location', methods: ['POST', 'HEAD'])]
+    public function addLocation(ManagerRegistry $doctrine, Request $request, JsonSerializer $JsonSerializer): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $data = $request->getContent();
+        $location = $JsonSerializer->deserializeJson($data, Locations::class);
+        $entityManager->persist($location);
+        $entityManager->flush();
+        return new Response($data);
+    }
 }
