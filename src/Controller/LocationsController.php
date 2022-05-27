fd5548d68db5729ea069e50bb34b978b79180db1
msirisko <mirja.sirisko@adesso.de>
2022-05-27 03:30:35 -0700

    byContinent working

--- src/Controller/LocationsController.php
+++ src/Controller/LocationsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Locations;
+use App\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,34 +32,12 @@ class LocationsController extends AbstractController
         $entityManager->flush();
         return new Response($data);
     }
-    #[Route('/locations/{continent}', name: 'get_location_byContinent', methods: ['GET', 'HEAD'])]
-
-   /* public function showLocationByContinent(Request $request)
+    // existing URLS: '/locations/Europa', '/locations/Nord-Amerika', 'locations/Asien-Pazifik
+    #[Route('/locations/{continent}', name: 'show_locations_byContinent', methods: ['GET', 'HEAD'])]
+    public function showLocationsByContinent($continent, ManagerRegistry $doctrine, JsonSerializer $JsonSerializer): Response
     {
-        $q = $request->query->get('continent');
-        // Get standard repository
-        $user = $this->getDoctrine()->getManager()
-            ->getRepository(Locations::class)
-            ->findBy(['continent' => $q]); // Or use the magic method ->findByEmail($q);
-
-        // Present your results
-        return $this->render('admin/search/search_results.html.twig',
-            ['location' => $user]);
-    } */
-    public function findByContinent(string $continent, ManagerRegistry $doctrine): ?Locations{
-       $entityManager = $doctrine->getManager();
-        return $entityManager->createQuery(
-            'SELECT c
-             FROM App\Entity\Locations c
-             WHERE c.continent LIKE :continent'
-
-        )
-            ->setParameters([
-                ':continent' => '%'.$continent.'%',
-            ])
-            ->getSingleResult()
-            ;
-
-        }
-
+        $data = $doctrine->getRepository(Locations::class)->findBy(['continent' => $continent]);
+        $jsonContent = $JsonSerializer->serializeJson($data);
+        return new Response($jsonContent);
+    }
 }
